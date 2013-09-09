360f665e87c5eed46cda68f6ffc540f9e0ab8701
LAN Xingcan <lanxingcan@gmail.com>
2013-09-09 12:41:56 +0800

    Add testcases
    Signed-off-by: LAN Xingcan <lanxingcan@gmail.com>

---
+++ test/singleton-01.cpp
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2013 LAN Xingcan
+ * All right reserved
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <spin/enable_singleton.hpp>
+#include <cassert>
+
+class singleton : public spin::enable_singleton<singleton>
+{
+public:
+  static singleton_factory get_instance;
+};
+
+
+int main()
+{
+  std::shared_ptr<singleton> x = singleton::get_instance();
+  assert(x);
+  return 0;
+}
